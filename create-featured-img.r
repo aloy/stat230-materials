71cae6684d885d2da9b25adb9ad09d4228e86c83
Adam Loy <loyad01@gmail.com>
2025-09-14 14:12:21 -0500

    posting day 1 slides

---
+++ create-featured-img.r
@@ -0,0 +1,10 @@
+# Create a screenshot of the featured post for sharing on webpage
+deck <- "01-slides-slr-review"
+quarto::quarto_render(paste0("slides/", deck, ".qmd"))
+webshot2::webshot(
+  url = paste0("docs/slides/", deck, ".html"),
+  file = paste0("slides/featured/", deck, ".png"),
+  vwidth = 1920,
+  vheight = 1080
+)
+# unlink(paste0("docs/slides/", deck, ".html"))
