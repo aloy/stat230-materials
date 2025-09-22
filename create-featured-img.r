823fb20449234c2ff26e76271d60524732e2cce5
Adam Loy <loyad01@gmail.com>
2025-09-22 09:31:21 -0500

    slides + archive updated

--- create-featured-img.r
+++ create-featured-img.r
@@ -1,5 +1,5 @@
 # Create a screenshot of the featured post for sharing on webpage
-deck <- "02-slr-inference"
+deck <- "03-slr-prediction"
 quarto::quarto_render(paste0("slides/", deck, ".qmd"))
 webshot2::webshot(
   url = paste0("docs/slides/", deck, ".html"),
