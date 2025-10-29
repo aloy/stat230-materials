1e65f6cf038e31694b714fb62635fd498df6c73c
Adam Loy <loyad01@gmail.com>
2025-10-29 08:58:09 -0500

    posting slides

--- create-featured-img.r
+++ create-featured-img.r
@@ -1,5 +1,5 @@
 # Create a screenshot of the featured post for sharing on webpage
-deck <- "14-binary-logistic"
+deck <- "15-logistic-inference"
 quarto::quarto_render(paste0("slides/", deck, ".qmd"))
 webshot2::webshot(
   url = paste0("docs/slides/", deck, ".html"),
