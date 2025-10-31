900ea51367c27b7ee72a97ae50a0588df1b225a2
Adam Loy <loyad01@gmail.com>
2025-10-30 22:30:02 -0500

    posting logistic regression slides diagnostics

--- create-featured-img.r
+++ create-featured-img.r
@@ -1,5 +1,5 @@
 # Create a screenshot of the featured post for sharing on webpage
-deck <- "15-logistic-inference"
+deck <- "16-logistic-diagnostics"
 quarto::quarto_render(paste0("slides/", deck, ".qmd"))
 webshot2::webshot(
   url = paste0("docs/slides/", deck, ".html"),
