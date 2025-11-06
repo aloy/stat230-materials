d9e6331a235484a06986ddcde753c85b9766bb15
Adam Loy <loyad01@gmail.com>
2025-11-06 09:44:52 -0600

    quasibinomial slide deck

--- create-featured-img.r
+++ create-featured-img.r
@@ -1,5 +1,5 @@
 # Create a screenshot of the featured post for sharing on webpage
-deck <- "16-logistic-diagnostics"
+deck <- "18-binomial-gof"
 quarto::quarto_render(paste0("slides/", deck, ".qmd"))
 webshot2::webshot(
   url = paste0("docs/slides/", deck, ".html"),
